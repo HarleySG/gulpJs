9ebff72fee3cd0bf70966c31cb4f66976c68614e
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2019-11-06 17:02:26 -0500

    new: gulp/notify.js

---
+++ gulp/notify.js
@@ -0,0 +1,13 @@
+const notify = require('gulp-notify');
+
+module.exports = function(error) {
+    var args = Array.prototype.slice.call(arguments);
+    notify
+        .onError({
+            title: 'Compile Error',
+            message: '<%= error.message %>',
+        })
+        .apply(this, args);
+
+    this.emit('end');
+};
