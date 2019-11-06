646fc405001a271572dc17b3081aeb741b5a31f7
malopez1578 <malopez1578@gmail.com>
2019-11-06 08:18:10 -0500

    Update: se realizan cambios para pruebas de ruta en compilación del pug

--- gulp/pug.js
+++ gulp/pug.js
@@ -7,28 +7,27 @@ const pug = require('gulp-pug');
 const pugInheritance = require('gulp-pug-inheritance');
 
 function setPug(origin, destiny) {
-    return (
-        src(origin)
-            .pipe(changed('dist', { extension: '.html' }))
-            .pipe(gulpif(global.isWatching, cached('setPug')))
-            // .pipe(
-            //     pugInheritance({
-            //         basedir: `${origin}`,
-            //         skip: 'node_modules',
-            //     })
-            // )
-            .pipe(
-                filter(function(file) {
-                    return !/\/_/.test(file.path) && !/^_/.test(file.relative);
-                })
-            )
-            .pipe(
-                pug({
-                    pretty: true,
-                }).on('error', $.hasError)
-            )
-            .pipe(dest(destiny))
-    );
+    return src(origin, { since: $.gulp.lastRun(setPug) })
+        .pipe(changed('dist', { extension: '.html' }))
+        .pipe(gulpif(global.isWatching, cached(setPug)))
+        .pipe(
+            pugInheritance({
+                basedir: `src/views`,
+                skip: 'node_modules',
+            })
+        )
+
+        .pipe(
+            filter(function(file) {
+                return !/\/_/.test(file.path) && !/^_/.test(file.relative);
+            })
+        )
+        .pipe(
+            pug({
+                pretty: true,
+            }).on('error', $.hasError)
+        )
+        .pipe(dest('dist'));
 }
 
 module.exports = setPug;
